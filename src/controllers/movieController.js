9a165f95dc9eb04a447ad2bf50163a086e8fea2c
mac <aidahimmiche@gmail.com>
2022-01-30 15:43:52 +0100

    commit

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -4,7 +4,19 @@ const moviesService = require("../data/services/movieService");
 async function getMovie (req, res) {
     try {
         let movie = await moviesService.getMovie();
-        return res.status(200).json({ movie: movie, message: "Service Executed Successfully" })
+        return res.status(200).json({ movie: movie, message: "Task executed successfully"});
+        //json({ movie: movie, message: "Service Executed Successfully" })
+
+    } catch (e) {
+        return res.status(400).json({ status: 400, message: e.message });
+    }
+}
+
+async function getAllMovies (req, res) {
+    try {
+        let movies = await moviesService.getAllMovies();
+        return res.render('index', { movies: movies, title: 'All movies' });
+        //json({ movie: movie, message: "Service Executed Successfully" })
 
     } catch (e) {
         return res.status(400).json({ status: 400, message: e.message });
@@ -12,6 +24,4 @@ async function getMovie (req, res) {
 }
 
 
-
-
-module.exports = {getMovie};
+module.exports = {getMovie, getAllMovies};
