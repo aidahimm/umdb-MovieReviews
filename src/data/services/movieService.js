82a71ec9726ddbf50b2f2464c86233e10a0891c1
mac <aidahimmiche@gmail.com>
2022-02-03 16:13:26 +0100

    commit

--- src/data/services/movieService.js
+++ src/data/services/movieService.js
@@ -1,18 +1,9 @@
 const mongoDriver = require("../../Mongo");
 const request = require("request");
+const Movie = require("../models/movie");
 
 var element = {};
 var posters = [];
-
-async function getMovie(){
-    try {
-        // Connect to the MongoDB cluster
-        let db = await mongoDriver.mongo();
-        return await db.collection("movies").findOne({movieID: 1});
-    } catch (e) {
-        console.log(e);
-    }
-}
 
 async function getBody(body){
     let json = JSON.parse(body);
@@ -46,5 +37,87 @@ async function getHomeMovies(){
     }
 }
 
-module.exports = {getMovie, getHomeMovies};
+async function findMovieByID(id){
+    try {
+        // Connect to the MongoDB cluster
+        let db = await mongoDriver.mongo();
+        return await db.collection("movies").findOne({movieID: parseInt(id)});
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+async function findMovieByTitle(title){
+    try{
+        let db = await mongoDriver.mongo();
+        return await db.collection("movies").find({title:{'$regex' : title, '$options' : 'i'}}).toArray();
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+async function findMovieByGenre(genre){
+    try{
+        let db = await mongoDriver.mongo();
+        return await db.collection("movies").find({genres:{'$regex' : genre, '$options' : 'i'}}).toArray();
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+async function createMovie (title, genres, imdbId, avgr, numv){
+    try{
+        let db = await mongoDriver.mongo();
+        let tot = await db.collection("movies").count() +1;
+        let newMovie = new Movie({
+            movieID: tot,
+            title: title,
+            genres: genres,
+            imdbId: imdbId,
+            avgRating: avgr,
+            numVotes: numv
+        })
+        await db.collection("movies").insertOne(newMovie);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+async function updateMovieRating (movieID, rating) {
+    try{
+        let movie = await findMovieByID(movieID);
+        let newAvgRating = ((movie.avgRating * movie.numVotes)+ parseInt(rating))/ (movie.numVotes +1);
+        let newNumVotes = (movie.numVotes +1);
+        let update = {avgRating: parseInt(newAvgRating.toFixed(1)), numVotes: newNumVotes};
+
+        let db = await mongoDriver.mongo();
+        await db.collection("movies").updateOne({movieID: parseInt(movieID)}, {$set: update});
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+async function deleteMovie (id){
+    try{
+        let db = await mongoDriver.mongo();
+        await db.collection("movies").deleteOne({movieID: parseInt(id)});
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+//Find how many watching lists a movie was added to
+async function MovieInLWatchlists(id){
+    try{
+        //filter watching lists by movieID
+        //get number of matched watching list documents
+        //return number [popularity of the movie]
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+
+
+module.exports = {getHomeMovies, findMovieByID,findMovieByTitle, findMovieByGenre, createMovie, updateMovieRating, deleteMovie};
 
--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -1,24 +1,34 @@
 const express = require('express');
 const router = express.Router();
 const movieController = require('../controllers/movieController');
-var request = require('request');
 
 router.get('/movie', (req,res)=>{
-    movieController.getMovie(req,res).then();
-});
-
-router.get('/apimovie', function(req, res) {
-    request({
-        uri: 'http://www.omdbapi.com/?',
-        qs: {
-            apikey: '6fe3a4fe',
-            i: "tt4154796"
-        }
-    }).pipe(res);
+    movieController.findMovieByID(req,res).then();
 });
 
 router.get('/movies', (req,res)=>{
     movieController.getHomeMovies(req,res).then();
 });
+
+router.post('/tsearch', (req,res)=>{
+    movieController.findMovieByTitle(req,res).then();
+});
+
+router.post('/gsearch', (req,res)=>{
+    movieController.findMovieByGenre(req,res).then();
+});
+
+router.post('/crtmov', (req,res)=>{
+    movieController.createMovie(req,res).then();
+});
+
+router.put('/updmovr', (req,res)=>{
+    movieController.updateMovieRating(req,res).then();
+});
+
+router.delete('/delmov', (req,res) =>{
+    movieController.deleteMovie(req,res).then();
+})
+
 
 module.exports = router;
